1f7e3e75fe6c6c7f36bf0709a03d2c2af676f894
Your Name <you@example.com>
2020-03-22 18:23:49 +0200

    wordpress

---
+++ wordpress/wp-content/themes/garmony/footer.php
@@ -0,0 +1,33 @@
+
+<!-- FOOTER -->
+
+<section class="footer">
+  <div class="footer-content">
+    <div class="footer-logo">
+      <img src="<?php echo get_template_directory_uri().'/media/image/footer-logo.png'; ?>" alt="">
+      <div class="footer-contact">
+        <img src="<?php echo get_template_directory_uri().'/media/icon/call.png'; ?>" alt="">
+        <p>(8712) 22-30-42</p>
+      </div>
+    </div>
+  </div>
+  <div class="footer-content">
+  <nav class="anhors">
+      <ul>
+        <li><a href="#products">Продукция</a></li>
+        <li><a href="#about">О нас</a></li>
+        <li><a href="#benefits">Преимущества</a></li>
+        <li><a href="#contact">Контакты</a></li>
+      </ul>
+    </nav>
+    <p>© Администрация Президента России 2020</p>
+  </div>
+</section>
+
+
+
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wordpress/wp-content/themes/garmony/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap-grid.css'>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,900&display=swap" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="css/magnific-popup.css">
+  <link rel="stylesheet" href="css/main.css"> -->
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php wp_head(); ?>
+  <title><?php the_title(); ?></title>
+</head>
+<body>
